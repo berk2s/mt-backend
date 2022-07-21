46ac1d9a1a598b12f417d334d8755377f0ccca89
berk2s <berktopcu@outlook.com>
2022-07-21 23:35:36 +0300

    added update profile photo endpoint

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -4,10 +4,15 @@
 
 import { RegisterAthleteRequest } from '@app/controllers/athlete-controller/athlete-controller.types'
 import { DocumentExists } from '@app/exceptions/document-exists-error'
+import { DocumentNotFound } from '@app/exceptions/document-not-found-error'
 import { UserMapper } from '@app/mappers/user.mapper'
 import { AthleteUser } from '@app/model/user/Athlete'
 import { BaseUser, BaseUserModel } from '@app/model/user/BaseUser'
 import { UserResponse } from '@app/types/response.types'
+import { ObjectIdUtility } from '@app/utilities/objectid-utility'
+import { ObjectId } from 'mongoose'
+import { loggers } from 'winston'
+import imageService from '../image/image.service'
 import loggerService from '../logger/logger-service'
 
 /**
@@ -22,12 +27,14 @@ class UserService {
     this.baseUserModel = BaseUser
   }
 
+  /**
+   * Registers athlete user type to the database
+   */
   public async registerAthlete(
     registerUser: RegisterAthleteRequest,
   ): Promise<UserResponse> {
     const {
       fullName,
-      imageUrl,
       email,
       password,
       birthday,
@@ -48,7 +55,6 @@ class UserService {
 
     const athlete = new AthleteUser({
       fullName,
-      imageUrl,
       email,
       passwordHash: password,
       birthDate: birthday,
@@ -63,6 +69,39 @@ class UserService {
     loggerService.info(`User has been created [userId: ${athlete._id}]`)
 
     return Promise.resolve(UserMapper.userToDTO(athlete))
+  }
+
+  /**
+   * Updates user profile photo
+   */
+  public async updateProfilePhoto(
+    userId: string,
+    buffer: any,
+  ): Promise<UserResponse> {
+    if (!ObjectIdUtility.isValid(userId)) {
+      loggerService.warn(`Given object id is invalid [id: ${userId}]`)
+      throw new DocumentNotFound('user.notFound')
+    }
+
+    const user = await this.baseUserModel.findById(userId)
+
+    if (!user) {
+      loggerService.warn(
+        `User with given ID does not exists [userId: ${userId}]`,
+      )
+      throw new DocumentNotFound('user.notFound')
+    }
+
+    const fileName = await imageService.save(buffer)
+
+    user.imageUrl = fileName
+    user.save()
+
+    loggerService.info(
+      `User profile photo has been updated. [userId: ${user._id}]`,
+    )
+
+    return Promise.resolve(UserMapper.baseUsertoDTO(user))
   }
 }
 
